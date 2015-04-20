85a4e617f64e3c0f9761d894de50a01cae561897
stopbox <disneydisney4@gmail.com>
2015-04-20 17:31:51 -0500

    Prepare for Heavy work

--- src/main/java/me/stopbox123/server/MusicAPI.java
+++ src/main/java/me/stopbox123/server/MusicAPI.java
@@ -1,24 +1,13 @@
 package me.stopbox123.server;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.URL;
-
-import me.stopbox123.gzip.UnZip;
-import me.stopbox123.web.WebServer;
 import me.stopbox123.web.WebsocketServer;
-
-import org.apache.commons.io.FileUtils;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.FileUtil;
+
+import java.io.IOException;
 
 public class MusicAPI extends JavaPlugin {
 	
 	private static MusicAPI instance;
-
-    File f;
 
 	@Override
 	public void onEnable() {
@@ -27,32 +16,13 @@ public class MusicAPI extends JavaPlugin {
         saveDefaultConfig();
         saveConfig();
 
-        f = new File(getDataFolder() + "/htdocs/MCSrv.zip");
-
-        Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-            public void run() {
-                if (!f.exists()) {
-                    try {
-                        FileUtils.copyURLToFile(new URL("http://download1323.mediafire.com/xkvl169878tg/rxit3u3hauqfhs1/MCWebSrv.zip"), f);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }, 20L);
-
-        Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-            public void run() {
-                UnZip unzip = new UnZip();
-                unzip.unZip(getDataFolder() + "/htdocs/MCSrv.zip", getDataFolder() + "/htdocs");
-            }
-        }, 40L);
-
-		try {
-			WebsocketServer.runServer();
-            WebServer.runServer();
-		} catch (Exception e) {
-		}
+        try {
+            WebsocketServer.runServer();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 	public static MusicAPI getInstance() {
